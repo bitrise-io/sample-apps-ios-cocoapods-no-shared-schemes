82a605817553e6bef0e173a7c7fb9e8055ecf22f
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-08-23 14:33:33 +0200

    project init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# sample-apps-ios-cocoapods-no-shared-schemes
+sample-apps-ios-cocoapods
+=========================
+
+A sample iOS app which uses CocoaPods
+
+# Notes
+
+* with Xcode 6.0.1 and CocoaPods 0.34.2 I had to set "Build Active Architectures Only" to *No* in the Pods project!
+	* more info: [http://stackoverflow.com/a/22336208/974381](http://stackoverflow.com/a/22336208/974381)
+	* this means the Pods project file at *SampleAppWithCocoapods/Pods/Pods.xcodeproj/project.pbxproj* have to be included in the repository
---
+++ SampleAppWithCocoapods/SampleAppWithCocoapods/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  SampleAppWithCocoapods
+//
+//  Created by Viktor Benei on 10/15/14.
+//  Copyright (c) 2014 Bitrise. All rights reserved.
+//
+
+#import "ViewController.h"
+
+//#import "F.h"
+//#import "NSArray+F.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//     NSArray *numberArray = [NSArray arrayFrom:1 To:5];
+//    NSLog(@"numberArray: %@", numberArray);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
